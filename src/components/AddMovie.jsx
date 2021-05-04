566f3d9b9392eda36d88f07f1f5774455e1e268e
Fernando <fhomaia@gmail.com>
2021-05-04 16:22:23 -0300

    Erros Lint

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,6 @@
 // implement AddMovie component here.
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   constructor() {
@@ -41,57 +41,64 @@ class AddMovie extends React.Component {
 
   createform(title, type, alternative, name) {
     return (
-    <div>
-    <label data-testid={`${alternative}-input-label`} htmlFor={ `${alternative}-input` }>{name}
-    <input
-      type={ type }
-      name={ title }
-      value={ this.state[title] }
-      id={ `${alternative}-input` }
-      data-testid={ `${alternative}-input` }
-      onChange={ this.changeHandler }
-    />
-    </label>
-    </div>);
+      <div>
+        <label data-testid={ `${ alternative }-input-label` }
+          htmlFor={ `${ alternative }-input` }>
+          { name }
+          <input
+            type={ type }
+            name={ title }
+            value={ this.state[title] }
+            id={ `${alternative}-input` }
+            data-testid={ `${alternative}-input` }
+            onChange={ this.changeHandler }
+          />
+        </label>
+      </div>);
   }
 
   render() {
-    const { storyline, genre } = this.state
+    const { storyline, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-          {this.createform('subtitle', 'text', 'subtitle', 'Subtítulo' )}
-          {this.createform('title', 'text', 'title', 'Título')}
-          {this.createform('imagePath', 'text', 'image', 'Imagem')}
+        {this.createform('subtitle', 'text', 'subtitle', 'Subtítulo')}
+        {this.createform('title', 'text', 'title', 'Título')}
+        {this.createform('imagePath', 'text', 'image', 'Imagem')}
         <div>
-          <label data-testid="storyline-input-label" htmlFor="storyline-input">Sinopse</label>
-          <textarea
-            name="storyline"
-            value={ storyline }
-            id="storyline-input"
-            data-testid="storyline-input"
-            onChange={ this.changeHandler }
+          <label data-testid="storyline-input-label"
+            htmlFor="storyline-input">
+            Sinopse
+            <textarea
+              name="storyline"
+              value={ storyline }
+              id="storyline-input"
+              data-testid="storyline-input"
+              onChange={ this.changeHandler }
           />
+          </label>
         </div>
-          {this.createform('rating', 'number', 'rating', 'Avaliação')}
+        {this.createform('rating', 'number', 'rating', 'Avaliação')}
         <div>
-          <label data-testid="genre-input-label" htmlFor="genre-input">Gênero</label>
-          <select
-            id="genre-input"
-            data-testid="genre-input"
-            name="genre"
-            value={ genre }
-            onChange={ this.changeHandler }
-          >
-            <option value="action" data-testid="genre-option">Ação</option>
-            <option value="comedy" data-testid="genre-option">Comédia</option>
-            <option value="thriller" data-testid="genre-option">Suspense</option>
+          <label data-testid="genre-input-label" htmlFor="genre-input">
+            Gênero
+            <select
+              id="genre-input"
+              data-testid="genre-input"
+              name="genre"
+              value={ genre }
+              onChange={ this.changeHandler }
+            >
+              <option value="action" data-testid="genre-option">Ação</option>
+              <option value="comedy" data-testid="genre-option">Comédia</option>
+              <option value="thriller" data-testid="genre-option">Suspense</option>
           </select>
+          </label>
         </div>
         <button
           type="button"
           data-testid="send-button"
           onClick={ (event) => this.addMovieEvent(event) }
-        >
+          >
         Adicionar filme
         </button>
       </form>
@@ -100,7 +107,7 @@ class AddMovie extends React.Component {
 }
 
 AddMovie.propTypes = {
-  onClick: PropTypes.func
-}.isRequired
+  onClick: PropTypes.func,
+}.isRequired;
 
 export default AddMovie;
